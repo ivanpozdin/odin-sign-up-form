10db0b7d175cdde48e7bf7a4f1809ff6f27bc7c1
ivanpozdin <ivanllpozdin@gmail.com>
2023-08-23 14:53:52 +0400

    Create html + css for registration page

---
+++ script.js
@@ -0,0 +1,12 @@
+// const inputFields = [...document.querySelectorAll(".inputs input")];
+
+// inputFields.forEach((input) => {
+//   input.addEventListener("change", (e) => {
+//     console.log(input.getAttribute("aria-invalid"));
+//     console.log(input);
+//     if (input.getAttribute("aria-invalid"))
+//       input.closest("div").querySelector("p").classList.add("error");
+//     else input.closest("div").querySelector("p").classList.remove("error");
+//     console.log("remove error");
+//   });
+// });
